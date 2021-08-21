68bec3177d37b637d7e57acb343f98f2931c6d2b
Your NameLibolir <libomir.drobush@gmail.com>
2021-08-21 19:56:48 +0300

    new file:   package-lock.json 	new file:   src/API.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import './App.css'
+import structure from './API'
 
-function App() {
+const Contryes = ({ name, languages}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  let useClickOutside = (handler) => {
+    let tree = useRef()
+
+    useEffect(() => {
+      let mhandler = (e) => {
+        if(!tree.current.contains(e.target)){
+          handler()
+        }
+      }
+      document.addEventListener("mousedown", mhandler)
+      return () => {document.addEventListener("mousedown", mhandler)}  
+    })
+    return tree
+  }
+
+  let tree = useClickOutside(() => {
+    setIsActive(false)
+  })
+
+  return(
+    <ul ref={tree}>
+      <li ><span onClick={() => setIsActive(!isActive)}>{name}</span>
+        {isActive && <ul>
+          {languages.map(c => 
+            <li><span>{c.name}</span></li>
+          )}
+        </ul>}
+      </li>
+    </ul>
+  )
+}
+
+const Countries = ({name, countries}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  let useClickOutside = (handler) => {
+    let tree = useRef()
+
+    useEffect(() => {
+      let mhandler = (e) => {
+        if(!tree.current.contains(e.target)){
+          handler()
+        }
+      }
+      document.addEventListener("mousedown", mhandler)
+      return () => {document.addEventListener("mousedown", mhandler)}  
+    })
+    return tree
+  }
+
+  let tree = useClickOutside(() => {
+    setIsActive(false)
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul ref={tree} >
+        <li><span onClick={() => setIsActive(!isActive)}>{name}</span>
+          {isActive && <div>
+            {countries.map(c => <Contryes name={c.name} languages={c.languages} />)}
+          </div>}
+        </li>
+    </ul>
   );
+};
+
+
+const App=()=> {
+  return <div>{structure.data.continents.map(c =><Countries name={c.name} countries={c.countries} />)}</div>
 }
 
 export default App;
